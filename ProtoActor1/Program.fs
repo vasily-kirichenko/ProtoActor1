08e04b1f5e57903b647cb356398e59134158d107
Vasily Kirichenko <vaskir@gmail.com>
2017-11-19 16:18:54 +0300

    cleanup

--- ProtoActor1/Program.fs
+++ ProtoActor1/Program.fs
@@ -11,38 +11,40 @@ exception FatalException
 type ChildActor() =
     interface IActor with
         member __.ReceiveAsync ctx =
-            match ctx.Message with
-            | :? Hello as r -> printfn "Hello %A" r.Who
-            | :? Recoverable -> raise RecoverableException
-            | :? Fatal -> raise FatalException
-            | :? Started -> printfn "Started, initialize actor here"
-            | :? Stopping -> printfn "Stopping, actor is about shut down"
-            | :? Stopped -> printfn "Stopped, actor and it's children are stopped"
-            | :? Restarting -> printfn "Restarting, actor is about restart"
-            | _ -> printfn "Unexpected message %O" ctx.Message
-            Actor.Done
+            async {
+                match ctx.Message with
+                | :? Hello as r -> printfn "Hello %A" r.Who
+                | :? Recoverable -> raise RecoverableException
+                | :? Fatal -> raise FatalException
+                | :? Started -> printfn "Started, initialize actor here"
+                | :? Stopping -> printfn "Stopping, actor is about shut down"
+                | :? Stopped -> printfn "Stopped, actor and it's children are stopped"
+                | :? Restarting -> printfn "Restarting, actor is about restart"
+                | _ -> printfn "Unexpected message %O" ctx.Message
+            }
+            |> Async.StartAsTask :> _
 
 type ParentActor() =
     interface IActor with
         member __.ReceiveAsync ctx =
-            let child =
-                ctx.Children
-                |> Option.ofObj
-                |> Option.bind Seq.tryHead
-                |> Option.defaultWith (fun _ ->
-                     Actor.FromProducer(fun _ -> upcast ChildActor()) |> ctx.Spawn)
-    
-            match ctx.Message with
-            | :? Hello -> child.Tell ctx.Message
-            | :? Recoverable -> child.Tell ctx.Message
-            | :? Fatal -> child.Tell ctx.Message
-            | :? Terminated as r -> printfn "Watched actor was Terminated, %O" r.Who
-            | _ -> printfn "Unextected message %O" ctx.Message
+            async {
+                let child =
+                    ctx.Children
+                    |> Seq.tryHead
+                    |> Option.defaultWith (fun _ ->
+                         Actor.FromProducer(fun _ -> upcast ChildActor()) |> ctx.Spawn)
         
-            Actor.Done
+                match ctx.Message with
+                | :? Hello -> child.Tell ctx.Message
+                | :? Recoverable -> child.Tell ctx.Message
+                | :? Fatal -> child.Tell ctx.Message
+                | :? Terminated as r -> printfn "Watched actor was Terminated, %O" r.Who
+                | _ -> printfn "Unextected message %O" ctx.Message
+            } 
+            |> Async.StartAsTask :> _
 
 module Decider =
-    let decide (_: PID) (reason: exn) =
+    let decide _ (reason: exn) =
         match reason with
         | :? RecoverableException -> SupervisorDirective.Restart
         | :? FatalException -> SupervisorDirective.Stop
@@ -57,7 +59,7 @@ let main _ =
     actor.Tell { Who = "Alex" }
     actor.Tell Recoverable
     actor.Tell Fatal
-    Thread.Sleep (TimeSpan.FromSeconds 5.)
+    Thread.Sleep (TimeSpan.FromSeconds 1.)
     actor.Stop()
     Console.ReadLine() |> ignore
     0
